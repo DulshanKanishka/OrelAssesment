23871db809d443dff481959cbb26b8372de26201
dulshankanishka <dulshan.kanishka1996@gmail.com>
2022-12-15 13:59:28 +0530

    home page and specific country details pages are done

--- assesment/src/pages/Country/Country.js
+++ assesment/src/pages/Country/Country.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from "react";
 import apiData from "../../services/apiService";
+import {Table} from "react-bootstrap";
+import classes from "./Country.module.css";
 
 
 function Country() {
-    const url =  window.location.href;
+    const url = window.location.href;
     const lastSegment = url.substring(url.lastIndexOf("/") + 1);
     const [country, setCountry] = useState([]);
 
+    // Get Specific countries by 3-letter code
     const getCountry = async () => {
         try {
             await apiData.searchCountryFromApi(lastSegment).then((data) => {
@@ -17,13 +20,156 @@ function Country() {
         }
     };
     useEffect(() => {
-        getCountry();
+        getCountry().then(r => []);
     }, [])
-    console.log('Country')
-    console.log(country)
-    console.log('Country')
+
+    // getcurrensy data
+    let currency = "";
+    if (country.length !== 0) {
+        for (let key in country[0].currencies) {
+            currency = country[0].currencies[key].symbol;
+        }
+    }
+
     return (
-        <div>fdgdf ghdfgh dfgh dfghd ghd fdghd fgdfghd ghdgh </div>
+        <div className="container">
+            <div className={classes.breadcromb_holder}>
+                <p className={classes.breadcromb_txt_main}><span className={classes.breadcromb_txt_span}>Search</span> / {country.length === 0 ? '' : country[0].name.official}</p>
+            </div>
+            <p className={classes.country_title}>{country.length === 0 ? '' : country[0].name.official}</p>
+            <div className="row specific-country-page-holder">
+                <div className="contry-collumn-set col-12 col-md-6 col-lg-4">
+                    <p>Names</p>
+                    <Table bordered hover>
+                        <tbody>
+                        <tr>
+                            <td>Common</td>
+                            <td>{country.length === 0 ? '' : country[0].name.common}</td>
+                        </tr>
+                        <tr>
+                            <td>Official</td>
+                            <td>{country.length === 0 ? '' : country[0].name.official}</td>
+                        </tr>
+                        <tr>
+                            <td>Common (Native)</td>
+                            {/*<td>{country.length === 0 ? '' : country[0].name.nativeName.ara.common}</td>*/}
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>Official (Native)</td>
+                            {/*<td>{country.length === 0 ? '' : country[0].name.nativeName.ara.official}</td>*/}
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>Alternative spellings</td>
+                            <td>{country.length === 0 ? '' : country[0].altSpellings.join(", ")}</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+
+                    <p>Codes</p>
+
+                    <Table bordered hover>
+                        <tbody>
+                        <tr>
+                            <td>ISO 3166-1 alpha-2</td>
+                            <td>{country.length === 0 ? '' : country[0].cca2}</td>
+                        </tr>
+                        <tr>
+                            <td>ISO 3166-1 alpha-3</td>
+                            <td>{country.length === 0 ? '' : country[0].cca3}</td>
+                        </tr>
+                        <tr>
+                            <td>ISO 3166-1 (numeric)</td>
+                            <td>{country.length === 0 ? '' : country[0].ccn3}</td>
+                        </tr>
+                        <tr>
+                            <td>International calling codes</td>
+                            <td>{country.length === 0 ? '' : country[0].idd.root + country[0].idd.suffixes}</td>
+                        </tr>
+                        <tr>
+                            <td>ISO 4217 currency code</td>
+                            <td>{currency}</td>
+                        </tr>
+                        <tr>
+                            <td>Top level domains</td>
+                            <td>{country.length === 0 ? '' : country[0].tld.join(", ")}</td>
+
+                        </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                <div className="contry-collumn-set col-12 col-md-6 col-lg-4">
+                    <p>Language</p>
+                    <Table bordered hover>
+                        <tbody>
+                        <tr>
+                            <td>Native language</td>
+                            {/*<td>{country.length === 0 ? '' : country[0].languages.join(", ")}</td>*/}
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>Languages</td>
+                            {/*<td>Mark</td>*/}
+                            <td>-</td>
+
+                        </tr>
+                        <tr>
+                            <td>sin</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>tam</td>
+                            <td>-</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+
+                    <p>Geography</p>
+
+                    <Table bordered hover>
+                        <tbody>
+                        <tr>
+                            <td>Region</td>
+                            <td>{country.length === 0 ? '' : country[0].region}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Subregion</td>
+                            <td>{country.length === 0 ? '' : country[0].subregion}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Capital</td>
+                            <td>{country.length === 0 ? '' : country[0].capital}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Demonym</td>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <td>Lat/Lan</td>
+                            <td>{country.length === 0 ? '' : country[0].latlng.join(", ")}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Area</td>
+                            <td>{country.length === 0 ? '' : country[0].area}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Land borders</td>
+                            <td>{country.length === 0 ? '' : country[0].borders.join(", ")}</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                <div className="contry-collumn-set contry-collumn-set-last col-12 col-md-12 col-lg-4">
+                    <img src={country.length === 0 ? '' : country[0].flags.png} alt=""/>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- assesment/src/pages/Home/Home.js
+++ assesment/src/pages/Home/Home.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from "react";
 import apiData from "../../services/apiService";
 import {Card, Col, Form, ListGroup, Row} from "react-bootstrap";
-import classes from "../../Layout/Header/Header.module.css";
+import classes from "./Home.module.css";
 import searchico from '../../Images/search.png';
-import {Link} from "react-router-dom";
 
 function Home() {
 
     const [countries, setCountries] = useState([]);
 
+    // Get all countries
     const getCountries = async () => {
         try {
             await apiData.getCountriessFromApi().then((data) => {
@@ -21,63 +21,57 @@ function Home() {
     useEffect(() => {
         getCountries();
     }, [])
-    console.log('Country')
-    console.log(countries)
-    console.log('Country')
 
     return (
         <div className="container">
-            <h1>Search for a country</h1>
+            <h1 className={classes.search_for_text}>Search for a country</h1>
             {/*{countries.map(function (country, id) {*/}
-            <Form.Control size="sm" type="text" placeholder="enter country name"/>
-            <Row xs={1} md={3} className="g-4">
+            <Form.Control className={classes.searchbar} size="sm" type="text" placeholder="enter country name"/>
+            <Row xs={1} md={2} lg={3} xl={4} className="g-4">
                 {/*{countries.map(function (country, id) {*/}
                 {Array.from(countries).map((country, id) => (
                     <Col Key={id}>
                         <Card>
-                            <Card.Header>
+                            <Card.Header className={classes.home_card_header}>
                                 <div className="row">
-                                    <div className="col-md-8">
-                                        <p>{country.name.official}</p>
-                                        <p>{country.name.common}</p>
+                                    <div className="col-9 col-md-8 col-xl-9">
+                                        <p className={classes.home_official_name}>{country.name.official}</p>
+                                        <p className={classes.home_common_name}>{country.name.common}</p>
                                     </div>
-                                    <div className="col-md-4">
-                                        <a className={classes.aboutLink} href={"/country?country=" + country.cca3}>
+                                    <div className="col-3 col-md-4 col-xl-3">
+                                        <a className={classes.view_country_detail_btn} href={"/country/" + country.cca3}>
                                             <img src={searchico} alt=""/>
                                         </a>
-                                        <Link component="country" to={"/country/" + country.cca3}>
-                                            Read doc
-                                        </Link>;
                                     </div>
                                 </div>
                             </Card.Header>
-                            <ListGroup variant="flush">
+                            <ListGroup className="home-list-groupe" variant="flush">
                                 <ListGroup.Item>
                                     <div className="row">
-                                        <div className="col-md-9">
+                                        <div className="col-9 col-md-9">
                                             <p>Country Code (Alpha 2)</p>
                                         </div>
-                                        <div className="col-md-3">
+                                        <div className="col-3 col-md-3">
                                             <p>{country.cca2}</p>
                                         </div>
                                     </div>
                                 </ListGroup.Item>
                                 <ListGroup.Item>
                                     <div className="row">
-                                        <div className="col-md-9">
+                                        <div className="col-9 col-md-9">
                                             <p>Country Code (Alpha 3)</p>
                                         </div>
-                                        <div className="col-md-3">
+                                        <div className="col-3 col-md-3">
                                             <p>{country.cca3}</p>
                                         </div>
                                     </div>
                                 </ListGroup.Item>
                                 <ListGroup.Item>
                                     <div className="row">
-                                        <div className="col-md-9">
+                                        <div className="col-9 col-md-9">
                                             <p>Currency</p>
                                         </div>
-                                        <div className="col-md-3">
+                                        <div className="col-3 col-md-3">
                                             <p>USD</p>
                                             {/*<p>{for (let key in country.currencies) {*/}
                                             {/*    country.currencies[key].name */}
@@ -89,20 +83,20 @@ function Home() {
 
                                 <ListGroup.Item>
                                     <div className="row">
-                                        <div className="col-md-9">
+                                        <div className="col-9 col-md-9">
                                             <p>int. dial code</p>
                                         </div>
-                                        <div className="col-md-3">
+                                        <div className="col-3 col-md-3">
                                             <p>{country.idd.root}{country.idd.suffixes}</p>
                                         </div>
                                     </div>
                                 </ListGroup.Item>
                                 <ListGroup.Item>
                                     <div className="row">
-                                        <div className="col-md-9">
+                                        <div className="col-9 col-md-9">
                                             <p>Language</p>
                                         </div>
-                                        <div className="col-md-3">
+                                        <div className="col-3 col-md-3">
                                             <p>eng</p>
                                         </div>
                                     </div>
